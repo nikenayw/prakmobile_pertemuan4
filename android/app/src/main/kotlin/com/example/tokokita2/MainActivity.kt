660c4f7b82cf7fbdac04177b994147793c491dce
nikenayw <nikenwjy23@gmail.com>
2024-10-04 00:39:17 +0700

    ya

---
+++ android/app/src/main/kotlin/com/example/tokokita2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tokokita2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bloc/logout_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:tokokita2/helpers/user_info.dart';
+
+class LogoutBloc {
+  static Future logout() async {
+    await UserInfo().logout();
+  }
+}
